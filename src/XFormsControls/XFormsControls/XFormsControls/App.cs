635ab081789c3a83c0b8e087a645d64aae2fba09
Evgeniy Pakalo <evgeniy.pakalo@gmail.com>
2015-03-11 21:08:37 +0300

    Added XAML page

--- src/XFormsControls/XFormsControls/XFormsControls/App.cs
+++ src/XFormsControls/XFormsControls/XFormsControls/App.cs
@@ -1,9 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-using Xamarin.Forms;
+﻿using Xamarin.Forms;
+using XFormsControls.Pages;
 
 namespace XFormsControls
 {
@@ -12,19 +8,7 @@ namespace XFormsControls
         public App()
         {
             // The root page of your application
-            MainPage = new ContentPage
-            {
-                Content = new StackLayout
-                {
-                    VerticalOptions = LayoutOptions.Center,
-                    Children = {
-                        new Label {
-                            XAlign = TextAlignment.Center,
-                            Text = "Welcome to Xamarin Forms!"
-                        }
-                    }
-                }
-            };
+            MainPage = new MainPage();
         }
 
         protected override void OnStart()
---
+++ src/XFormsControls/XFormsControls/XFormsControls/Pages/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using Xamarin.Forms;
+
+namespace XFormsControls.Pages
+{
+    public partial class MainPage : ContentPage
+    {
+        private string _mainText;
+
+        public string MainText
+        {
+            get { return _mainText; }
+            set
+            {
+                _mainText = value;
+                OnPropertyChanged();
+            }
+        }
+
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            BindingContext = this;
+
+            MainText = "Welcome to Xamarin Forms!";
+        }
+    }
+}
