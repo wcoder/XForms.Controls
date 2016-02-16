fa79e813f695bb3ec1dface8ceae64c57b9a5136
Yauheni Pakala <evgeniy.pakalo@gmail.com>
2016-02-16 23:26:17 +0300

    Added new controls & removed old

---
+++ src/XForms.Controls/CommandListView.cs
@@ -0,0 +1,40 @@
+﻿using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace XForms.Controls
+{
+	public class CommandListView : ListView
+	{
+		public static BindableProperty SelectedItemCommandProperty =
+				BindableProperty.Create<CommandListView, ICommand>(o => o.SelectItemCommand,
+					default(ICommand), BindingMode.OneWay, null,
+					null, null, null);
+
+		public ICommand SelectItemCommand
+		{
+			get { return (ICommand)GetValue(SelectedItemCommandProperty); }
+			set { SetValue(SelectedItemCommandProperty, value); }
+		}
+
+		public CommandListView()
+		{
+			ItemSelected += XListView_ItemSelected;
+		}
+
+		private void XListView_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+		{
+			var listView = sender as ListView;
+			if (listView != null)
+			{
+				if (SelectItemCommand != null)
+				{
+					if (listView.SelectedItem != null)
+					{
+						SelectItemCommand.Execute(listView.SelectedItem);
+						listView.SelectedItem = null;
+					}
+				}
+			}
+		}
+	}
+}
