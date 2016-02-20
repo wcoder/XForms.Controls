242ee76e17f38c49215d2b8e773c4863e65da588
Yauheni Pakala <evgeniy.pakalo@gmail.com>
2016-02-20 20:36:34 +0300

    Added TappedGestureAttached

--- samples/XForms.Controls.Samples/XForms.Controls.Samples/ViewModels/MainViewModel.cs
+++ samples/XForms.Controls.Samples/XForms.Controls.Samples/ViewModels/MainViewModel.cs
@@ -10,10 +10,12 @@ namespace XForms.Controls.Samples.ViewModels
 	public class MainViewModel : BaseViewModel
 	{
 		private Book _pickerSelectedBook;
+		private bool _isTouched;
 
 
 		public ObservableCollection<Book> Books { get; private set; }
 		public ICommand SelectBookCommand { get; private set; }
+		public ICommand TapCommand { get; private set; }
 
 		public Book PickerSelectedBook
 		{
@@ -21,11 +23,18 @@ namespace XForms.Controls.Samples.ViewModels
 			set { SetProperty(ref _pickerSelectedBook, value); }
 		}
 
+		public bool IsTouched
+		{
+			get { return _isTouched; }
+			set { SetProperty(ref _isTouched, value); }
+		}
+
 
 		public MainViewModel()
 		{
 			Books = new ObservableCollection<Book>();
 			SelectBookCommand = new Command<Book>(SelectBook);
+			TapCommand = new Command(() => { IsTouched = true; });
 
 			for (int i = 0; i < 20; i++)
 			{
---
+++ src/XForms.Controls/TappedGestureAttached.cs
@@ -0,0 +1,56 @@
+﻿using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace XForms.Controls
+{
+	/// <summary>
+	/// Author: Michael Ridland
+	/// http://www.michaelridland.com/xamarin/xaml-attached-properties-tricks-in-xamarin-forms/
+	/// </summary>
+	public class TappedGestureAttached
+	{
+		public static readonly BindableProperty CommandProperty =
+			BindableProperty.CreateAttached(
+				propertyName: "Command",
+				returnType: typeof(ICommand),
+				declaringType: typeof(View),
+				defaultValue: null,
+				defaultBindingMode: BindingMode.OneWay,
+				validateValue: null,
+				propertyChanged: OnItemTappedChanged);
+
+
+		public static ICommand GetItemTapped(BindableObject bindable)
+		{
+			return (ICommand)bindable.GetValue(CommandProperty);
+		}
+
+		public static void SetItemTapped(BindableObject bindable, ICommand value)
+		{
+			bindable.SetValue(CommandProperty, value);
+		}
+
+		public static void OnItemTappedChanged(BindableObject bindable, object oldValue, object newValue)
+		{
+			var control = bindable as View;
+
+			if (control != null)
+			{
+				control.GestureRecognizers.Clear();
+				control.GestureRecognizers.Add(
+					new TapGestureRecognizer
+					{
+						Command = new Command(o =>
+						{
+							var command = GetItemTapped(control);
+							if (command != null && command.CanExecute(null))
+							{
+								command.Execute(null);
+							}
+						})
+					}
+				);
+			}
+		}
+	}
+}
