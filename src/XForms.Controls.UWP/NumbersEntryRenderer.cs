6b1dfa965abe94488059e338c99a3d6a4050cc80
Yauheni Pakala <evgeniy.pakalo@gmail.com>
2016-02-21 01:52:05 +0300

    Added UWP project and NumbersEntryRenderer for UWP

---
+++ src/XForms.Controls.UWP/NumbersEntryRenderer.cs
@@ -0,0 +1,28 @@
+﻿using Windows.UI.Xaml.Input;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.UWP;
+using XForms.Controls;
+using XForms.Controls.UWP;
+
+[assembly: ExportRenderer(typeof(NumberEntry), typeof(NumbersEntryRenderer))]
+namespace XForms.Controls.UWP
+{
+	public class NumbersEntryRenderer : EntryRenderer
+	{
+		protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+		{
+			base.OnElementChanged(e);
+
+			if (e.OldElement == null)
+			{
+				var control = (FormsTextBox)Control;
+				InputScope inputScope = new InputScope();
+				InputScopeName inputScopeName = new InputScopeName();
+
+				inputScopeName.NameValue = InputScopeNameValue.Digits;
+				inputScope.Names.Add(inputScopeName);
+				control.InputScope = inputScope;
+			}
+		}
+	}
+}
