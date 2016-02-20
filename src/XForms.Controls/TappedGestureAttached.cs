67fb65e9ae9792520d8dcd0c726c9e0ee5c4a12f
Yauheni Pakala <evgeniy.pakalo@gmail.com>
2016-02-20 20:46:03 +0300

    Refactoring TappedGestureAttached

--- src/XForms.Controls/TappedGestureAttached.cs
+++ src/XForms.Controls/TappedGestureAttached.cs
@@ -17,40 +17,31 @@ namespace XForms.Controls
 				defaultValue: null,
 				defaultBindingMode: BindingMode.OneWay,
 				validateValue: null,
-				propertyChanged: OnItemTappedChanged);
+				propertyChanged: OnPropertyChanged);
 
+		public static void OnPropertyChanged(BindableObject bindable, object oldValue, object newValue)
+		{
+			var control = bindable as View;
+			if (control != null)
+			{
+				control.GestureRecognizers.Clear();
+				control.GestureRecognizers.Add(new TapGestureRecognizer
+				{
+					Command = new Command(o =>
+					{
+						var command = GetItemTapped(control);
+						if (command != null && command.CanExecute(null))
+						{
+							command.Execute(null);
+						}
+					})
+				});
+			}
+		}
 
 		public static ICommand GetItemTapped(BindableObject bindable)
 		{
 			return (ICommand)bindable.GetValue(CommandProperty);
-		}
-
-		public static void SetItemTapped(BindableObject bindable, ICommand value)
-		{
-			bindable.SetValue(CommandProperty, value);
-		}
-
-		public static void OnItemTappedChanged(BindableObject bindable, object oldValue, object newValue)
-		{
-			var control = bindable as View;
-
-			if (control != null)
-			{
-				control.GestureRecognizers.Clear();
-				control.GestureRecognizers.Add(
-					new TapGestureRecognizer
-					{
-						Command = new Command(o =>
-						{
-							var command = GetItemTapped(control);
-							if (command != null && command.CanExecute(null))
-							{
-								command.Execute(null);
-							}
-						})
-					}
-				);
-			}
 		}
 	}
 }
